0cba2305b27523cea1a8bb2bc534c9f4f49f3a22
cinexin <cats_dont_fly@yahoo.es>
2014-03-24 10:31:21 +0100

    Comparators in Java 8

---
+++ src/org/migue/java8tuto/tests/ComparatorTest.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package org.migue.java8tuto.tests;
+
+import static org.junit.Assert.*;
+
+import java.util.Comparator;
+
+import org.junit.Test;
+import org.migue.java8tuto.Person;
+
+/**
+ * @author migue
+ *
+ */
+public class ComparatorTest {
+
+	@Test
+	public void testComparators() {
+		
+		Comparator<Person> personComparator = (p1,p2) -> p1.getFirstName().compareTo(p2.getFirstName());
+		
+		Person p1 = new Person("John", "Doe");
+		Person p2 = new Person ("Alice", "Wonderland");
+		Person p3 = new Person ("Alice", "in Wonderland");
+		
+	 	assertTrue(personComparator.compare(p1, p2) > 0);
+	 	assertTrue(personComparator.reversed().compare(p1, p2) <0);
+	 	assertEquals(0, personComparator.compare(p2, p3));
+	}
+
+}
